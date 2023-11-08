04ec85106596b0b82925ea781e0a9e474ddcd001
brenda0323manon <brendamanon0526@gmail.com>
2023-11-07 20:38:55 -0500

    finished

---
+++ Problem #2/script.js
@@ -0,0 +1,41 @@
+// //Multiply by 3
+// function number(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i] * 3;
+//     }
+//     return arr;
+// }
+
+// const array = [1,2,3,4,5];
+// const multiply = number(array);
+// console.log(multiply);
+
+
+// //Adding by 1
+// function number(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i] + 1;
+//     }
+//     return arr;
+// }
+
+// const arrayAdd = [1,2,3,4,5];
+// const add = number(arrayAdd);
+// console.log(add);
+
+
+
+// //all are 10
+// function number(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i] = 10;
+//     }
+//     return arr;
+// }
+
+// const arrayEqual = [1,2,3,4,5];
+// const equal = number(arrayEqual);
+// console.log(equal);
+
+
+ 
