61993efcf36436b94b76028569937d6c559db188
brenda0323manon <brendamanon0526@gmail.com>
2023-11-09 15:17:19 -0500

    update

--- problem#5/script.js
+++ problem#5/script.js
@@ -1,18 +1,23 @@
 class Bookshelf {
     constructor() {
-    
+        this.books = [];
     }
 
     addBook(book) {
-        this.book = book
-        return this.book
+        this.books.push(book);
     }
 
+    getTotalBooks(){
+        return this.books.length
+    }
+    removeBook(book) {
+        const index = this.books.indexOf(book);
+        if(index !== -1) {
+            this.books.splice(index, 1);
+        }
+    }
 
 }
-
-
-
 
 
 const bookshelf = new Bookshelf(); 
@@ -21,8 +26,8 @@ bookshelf.addBook("To Kill a Mockingbird");
 bookshelf.addBook("Pride and Prejudice"); 
 
 
-// console.log(bookshelf.getTotalBooks()); // 3 
-// bookshelf.removeBook("To Kill a Mockingbird"); 
-// console.log(bookshelf.getTotalBooks()); // 2 
+console.log(bookshelf.getTotalBooks()); // 3 
+bookshelf.removeBook("To Kill a Mockingbird"); 
+console.log(bookshelf.getTotalBooks()); // 2 
 
-console.log(bookshelf);
+// console.log(bookshelf);
